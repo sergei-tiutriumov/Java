a6c37f165e80f74770b491e559a3398a662634b1
Serj <sergei.tiutriumov@gorfax.ru>
2025-07-03 17:59:33 +0300

    5

--- StartJava/Lesson_1/CyclesTheme.java
+++ StartJava/Lesson_1/CyclesTheme.java
@@ -27,7 +27,7 @@ public class CyclesTheme {
         }
 
         System.out.println("\n\n3. ВЫВОД ТАБЛИЦЫ УМНОЖЕНИЯ");
-        System.out.println("  | 2  3  4  5  6  7  8  9\n--+------------------------");
+        System.out.println("  | 2  3  4  5  6  7  8  9\n--+-----------------------");
         for (int i = 2; i <= 9; i++) {
             System.out.printf("%d |", i);
             for (int j = 2; j <= 9; j++) {
@@ -38,15 +38,33 @@ public class CyclesTheme {
         }
 
         System.out.println("\n\n4. ВЫВОД ЧИСЕЛ В НЕСКОЛЬКО СТРОК");
-        for (int i = 1; i < 30; i += 2) {
+        int limitNumber = 30;
+        int maxValue = 24;
+        for (int i = 1; i < limitNumber; i += 2) {
             if (((i - 9) % 10 == 0) && (i < 24)) {
                 System.out.printf("%2d\n", i);
-            } else if (i < 24) {
+            } else if (i < maxValue) {
                 System.out.printf("%2d ", i);
             } else {
                 System.out.printf("%2d ", 0);
             }
         }
         System.out.println();
+    
+        System.out.println("\n\n5. ВЫВОД ЧИСЕЛ МЕЖДУ MIN И MAX");
+        int[] numbers = {10, 5, -1};
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int swap = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = swap;
+                }
+            }
+        } 
+        for (int item : numbers) {
+            System.out.printf("%d ", item);
+        }
+        System.out.println("");
     }
 }
