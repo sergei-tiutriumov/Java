629edf628b4b3af790de54fddfbcf57a906b175b
Serj <sergei.tiutriumov@gorfax.ru>
2025-07-14 00:15:44 +0300

    калькулятор1

--- StartJava/Lesson_1/Calculator.java
+++ StartJava/Lesson_1/Calculator.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main (String[] args) {
+    public static int exponentiation(int number, int power) {
+        int result = 1;
+        for (int i = 0; i < power; i++) {
+            result *= number;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("Введите первое число ");
         int firstNumber = 0;
         int secondNumber = 0;
-        String operation = "";
         if (scanner.hasNextInt()) {
             firstNumber = scanner.nextInt();
             scanner.nextLine();
@@ -31,13 +38,13 @@ public class Calculator {
             secondNumber = scanner.nextInt();
         } 
         System.out.printf("%nКакое арифметическое действие? ");
-        operation = scanner.nextLine();
+        String operation = scanner.nextLine();
         if (operation.equals("+") ||
-            operation.equals("-") ||
-            operation.equals("*") ||
-            operation.equals("/") ||
-            operation.equals("^") ||
-            operation.equals("%")) {
+                operation.equals("-") ||
+                operation.equals("*") ||
+                operation.equals("/") ||
+                operation.equals("^") ||
+                operation.equals("%")) {
             System.out.printf("%n%d %d %s%n", firstNumber, secondNumber, operation); 
         } else {
             while (!(operation.equals("+") ||
@@ -46,12 +53,30 @@ public class Calculator {
                 operation.equals("/") ||
                 operation.equals("^") ||
                 operation.equals("%"))) {
-                System.out.println ("Нет такого арифметического действия!");
+                System.out.println("Нет такого арифметического действия!");
                 System.out.printf("Какое арифметическое действие? ");
                 operation = scanner.nextLine();
             }
-            System.out.printf("%n%d %d %s%n", firstNumber, secondNumber, operation); 
         }
-} 
-        
+        int result = -999999999;
+        if (operation.equals("^")) {
+            result = exponentiation(firstNumber, secondNumber);
+        }
+        if (operation.equals("+")) {
+            result = firstNumber + secondNumber;
+        }
+        if (operation.equals("-")) {
+            result = firstNumber - secondNumber;
+        }
+        if (operation.equals("*")) {
+            result = firstNumber * secondNumber;
+        }
+        if (operation.equals("/")) {
+            result = firstNumber / secondNumber;
+        }
+        if (operation.equals("%")) {
+            result = firstNumber % secondNumber;
+        }
+        System.out.printf("%n%d %s %d = %d %n", firstNumber, operation, secondNumber, result); 
+    } 
 }
