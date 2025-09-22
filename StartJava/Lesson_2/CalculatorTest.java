22694859b6f5fbd57e5fa0293e7faf61f21e4471
Serj <sergei.tiutriumov@gorfax.ru>
2025-09-22 23:30:19 +0300

    Person class

--- StartJava/Lesson_2/CalculatorTest.java
+++ StartJava/Lesson_2/CalculatorTest.java
@@ -4,7 +4,7 @@ public class CalculatorTest {
     public static class Calculator {
         public static float calcutate(CalculatorTest task) {
             float result = 0;
-            switch(task.operation) {
+            switch (task.operation) {
                 case "+":
                     result = task.firstNumber + task.secondNumber;
                     break;
@@ -30,66 +30,72 @@ public class CalculatorTest {
         }
     }
 
-     int firstNumber;
-     int secondNumber;
-     String operation;
-        public CalculatorTest() {
-            this.firstNumber = 0;
-            this.operation = null;
-            this.secondNumber = 0;
-        }
-        public  CalculatorTest(int firstNumber, String operation, int secondNumber) {
-            this.firstNumber = firstNumber;
-            this.operation = operation;
-            this.secondNumber = secondNumber;
-        }
-        public CalculatorTest getTask() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите первое число: ");
-
-            while (!scanner.hasNextInt()) {
-                System.out.println("Это не число!!!");
-                scanner.nextLine();
-                System.out.printf("Введите первое число ");
-            }
-            firstNumber = scanner.nextInt();
+    int firstNumber;
+    int secondNumber;
+    String operation;
+    
+    public CalculatorTest() {
+        this.firstNumber = 0;
+        this.operation = null;
+        this.secondNumber = 0;
+    }
+    
+    public CalculatorTest(int firstNumber, String operation, int secondNumber) {
+        this.firstNumber = firstNumber;
+        this.operation = operation;
+        this.secondNumber = secondNumber;
+    }
+    
+    public CalculatorTest getTask() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Это не число!!!");
             scanner.nextLine();
-            System.out.println("Введите знак операции (+, -, *, /, ^, %): ");
-            operation = scanner.nextLine();
-            String allowedOperations = "+ - * / ^ %";
-            while(!allowedOperations.contains(operation) || operation == "" || operation == " ") {
-                System.out.printf("Ошибка: операция %s не поддерживается \n", operation); 
-                System.out.println("Попробуйте еще раз ");
-                operation = scanner.nextLine();
-            }
-            System.out.println("Введите второе число: ");
-            while (!scanner.hasNextInt()) {
-                System.out.println("Это не число!!!");
-                scanner.nextLine();
-                System.out.printf("Введите второе число ");
-            }
-            secondNumber = scanner.nextInt();
-            return new CalculatorTest(firstNumber, operation, secondNumber);
-        }     
-        public static boolean isContinue() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Хотите продолжить вычисления? [yes/no]: ");
-            String answer = scanner.nextLine().toLowerCase().trim();
-            return answer.equals("yes") ? true : false;
+            System.out.printf("Введите первое число ");
         }
+        firstNumber = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Введите знак операции (+, -, *, /, ^, %): ");
+        operation = scanner.nextLine();
+        String allowedOperations = "+ - * / ^ %";
+        while (!allowedOperations.contains(operation) || operation == "" || operation == " ") {
+            System.out.printf("Ошибка: операция %s не поддерживается \n", operation); 
+            System.out.println("Попробуйте еще раз ");
+            operation = scanner.nextLine();
+        }
+        System.out.println("Введите второе число: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Это не число!!!");
+            scanner.nextLine();
+            System.out.printf("Введите второе число ");
+        }
+        secondNumber = scanner.nextInt();
+        return new CalculatorTest(firstNumber, operation, secondNumber);
+    }     
+    
+    public static boolean isContinue() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Хотите продолжить вычисления? [yes/no]: ");
+        String answer = scanner.nextLine().toLowerCase().trim();
+        return answer.equals("yes") ? true : false;
+    }
+    
     public static void main(String[] args) {
         CalculatorTest calc = new CalculatorTest();
         boolean continueCalculation = true;
-        while(continueCalculation) {
+        while (continueCalculation) {
             calc.getTask();
             if (calc.operation.equals("/") && calc.secondNumber == 0) {
                 System.out.println("Ошибка: деление на ноль запрещено!");
-                break;
-            }
-            float answer = Calculator.calcutate(calc);
-            System.out.printf("\n %d %s %d = %2f ", calc.firstNumber, calc.operation, calc.secondNumber, answer);
-            continueCalculation = isContinue();
+            } else {
+                float answer = Calculator.calcutate(calc);
+                System.out.printf("\n%d %s %d = %.2f ", calc.firstNumber,
+                        calc.operation,
+                        calc.secondNumber,
+                        answer);
+                continueCalculation = isContinue();
+            }    
         }
-        System.out.println("Это конец");
     }
 }
---
+++ StartJava/Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String sex = "male";
+    double age = 18;
+    double height = 1.8;
+    double weight = 70;
+    String name = "John";
+
+    public void walk() {
+        System.out.println("топ-топ-топ");
+    }
+
+    public void sit() {
+        System.out.println("сидит");
+    }
+
+    public void run() {
+        System.out.println("Вжух!!!!!!!!");
+    }
+
+    public void speak() {
+        System.out.println("Bla-bla-bla....");
+    }
+    
+    public void lernJava() {
+        System.out.println("Hello World!!!");
+    }
+}
